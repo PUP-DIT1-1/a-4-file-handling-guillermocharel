3aeeb441282dcde31dbf30e6431ac9a8b2d12d00
Guillermo, Charel V. <charelguillermo2025@gmail.com>
2026-03-23 16:15:13 +0800

    Add files via upload

---
+++ Python Student Record Saver/Python Student Record Saver.py
@@ -0,0 +1,35 @@
+while True: 
+    print("\nWelcome to the Student Record System!")
+    print("\nStudent Record Menu") 
+    print("1 - Add Student Record") 
+    print("2 - View Saved Records") 
+    print("3 - Exit") 
+
+    choice = input("Enter your choice (1-3): ") 
+    if choice == "1": 
+        name = input("Enter student name: ") 
+        grade = input("Enter student grade: ") 
+ 
+        with open("studentdata.txt", "a") as file: 
+            file.write(name + ", " + grade + "\n") 
+            print("Student record added successfully.") 
+ 
+    elif choice == "2": 
+        try: 
+            with open("studentdata.txt", "r") as file:
+                records = file.readlines()
+                if records:
+                    print("\nStudent Records:")
+                    for record in records:
+                        print(record.strip())
+                else:
+                    print("No student records found.")
+        except FileNotFoundError: 
+            print("No student records found.") 
+ 
+    elif choice == "3": 
+        print("\nExiting program... Thank you for using the Student Record System!") 
+        break 
+ 
+    else: 
+        print("Invalid choice. Please try again.") 
